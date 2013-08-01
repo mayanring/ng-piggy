812995e2e770755739e93e05200e3c214a6ec71a
Ryan Ming <ryan.ming@gmail.com>
2013-07-31 21:00:36 -0400

    split words and piglatinize each one

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,15 +10,29 @@ angular.module('ngPiggyApp')
   })
   .filter('piglatinize', function(){
     return function(text) {
+
+      var piglatinize = function( word ) {
+        if ( !word ) return "";
+
+        var pattern = /^([^aeiou]+)(.+)/i
+        var result = word.match( pattern );
+
+        if ( !result ){
+          return word + "ay";
+        }
+
+        return result[1] ? result[2] + result[1] + "ay" : word + "ay";
+      };
+
       if ( !text ) return;
 
-      var pattern = /^([^aeiou]+)(.+)/i
-      var result = text.match( pattern );
+      var words = text.split(/\s+/);
+      var latinized_words = [];
 
-      if ( !result ){
-        return text + "ay";
+      for( var i = 0; i < words.length; i++ ){
+        latinized_words.push( piglatinize( words[i] ) );
       }
 
-      return result[1] ? result[2] + result[1] + "ay" : text + "ay";
+      return latinized_words.join(" ");
     }
   });
